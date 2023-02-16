964f54d6697529ded77f7635e4a8997073066a19
Simon Davis <s.davis@hotmail.co.nz>
2023-02-16 19:07:51 +0000

    first draft of game plan, incomplete

---
+++ script.js
@@ -0,0 +1,61 @@
+//!ELEMENTS TO TARGET
+  //?Start button
+    //Will need to be able to target the start button to begin the game
+    //This should trigger a function that randomly places the computer's boats
+    //Should also enable the player to position their boats on the page, perhaps making the boards clickable
+
+  //?Rules div
+    //Text updates from rules of game to then contain text saying "Place your X" as player rotates through placing each ship
+    //Should contain fixed text that explains controls to rotate the boat around its axis
+
+  //?Grid squares
+    //First interaction with grid will be for player to place their boats. Probably only focus on one square at a time in terms of hover and clicks, but figure out a way to highlight multiple squares based on the size of the boat that's about to be placed (THINK ABOUT BOTH HOVER EVENTS AND CLICKS)
+    //At this point there'll need to be a function and listener that will rotate the boat 90deg so can be placed horizontally or vertically
+    //Grid squares will also need to be able to accept and track clicks in order to find enemy boats. (THIS WILL NEED TO WORK FOR BOTH PLAYER AND COMPUTER SHOTS)
+  
+  //?Boat placeholders (top and bottom of page)
+    //Boat placeholders will need to illuminate when you're placing the boat at the beginning of the game 
+    //and then again in a different colour when it has been destroyed
+
+//!GLOBAL VARIABLES
+
+  //?playerBoatsPlaced
+    //start at 0
+
+  //?playerBoatsRemaining
+    //number -> probs end up equalling playerBoatsPlaced once all boats are placed
+
+  //?compBoatsPlaced
+    //not sure about this one
+    //could be a number or a boolean
+
+  //?compBoatsRemaining
+    //number  
+  
+  
+
+//!PAGE-LOAD TASKS
+
+//!EXECUTE - functions
+
+  //?startGame
+
+  //?updateTextOnDivForBoatPlacement
+
+  //?placeBoat
+
+  //?rotateBoat
+
+  //?boatToSelect
+
+  //?boatDestroyed
+
+  //?compPlaceBoat
+
+  //?takeShot
+
+  
+
+
+
+//!EVENTS
