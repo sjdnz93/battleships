4030299576f0875dbea6a11e9ad11fc720fcc70e
Simon Davis <s.davis@hotmail.co.nz>
2023-02-20 11:43:40 +0000

    player can now visualise boat placement correctly. No repeated squares still highlighted. Boat selection divs now light up depending on which boat is being placed.

--- script.js
+++ script.js
@@ -14,7 +14,20 @@ function init() {
   const playerCells = []
   const compCells = []
 
-  //Boat divs
+  //Player boat divs
+  const pCarrier = document.querySelector('#pCarrier')
+  const pBattleship = document.querySelector('#pBattleship')
+  const pDestroyer = document.querySelector('#pDestroyer')
+  const pSubmarine = document.querySelector('#pSubmarine')
+  const pPatrol = document.querySelector('#pPatrol')
+
+  //Computer boat divs
+  const cCarrier = document.querySelector('#cCarrier')
+  const cBattleship = document.querySelector('#cBattleship')
+  const cDestroyer = document.querySelector('#cDestroyer')
+  const cSubmarine = document.querySelector('#cSubmarine')
+  const cPatrol = document.querySelector('#cPatrol')
+
   
   //Boat orientation
   let horizontal = true
@@ -37,8 +50,6 @@ function init() {
 
   console.log('GAME ACTIVE -->', gameActive)
   console.log('BOAT SELECTION -->', boatSelection)
-
-
 
   // !FUNCTIONS
 
@@ -64,71 +75,78 @@ function init() {
     gameActive = 1
     console.log('GAME ACTIVE -->', gameActive)
     console.log('BOAT SELECTION -->', boatSelection)
+    highlightBoatPic()
+    
+  }
+
+  //Highlight horizontal squares
+  function highlightHorizontal(e) {
+    const adjacentValues = [e.target.dataset.index - 1, e.target.dataset.index - 2, e.target.dataset.index - 3, e.target.dataset.index - 4, e.target.dataset.index - 5]
+    if (boatSelection === 1 && (e.target.dataset.index % width !== 0)){
+      e.target.classList.toggle('highlightOn')
+      playerCells[adjacentValues[0]].classList.toggle('highlightOn')
+    } else if (boatSelection === 2 && ((e.target.dataset.index - 1) % width !== 0 && e.target.dataset.index % width !== 0)){
+      e.target.classList.toggle('highlightOn')
+      playerCells[adjacentValues[0]].classList.toggle('highlightOn')
+      playerCells[adjacentValues[1]].classList.toggle('highlightOn')
+    } else if (boatSelection === 3 && ((e.target.dataset.index - 1) % width !== 0 && e.target.dataset.index % width !== 0 )){
+      e.target.classList.toggle('highlightOn')
+      playerCells[adjacentValues[0]].classList.toggle('highlightOn')
+      playerCells[adjacentValues[1]].classList.toggle('highlightOn')
+    } else if (boatSelection === 4 && ((e.target.dataset.index - 2) % width !== 0 && (e.target.dataset.index - 1) % width !== 0 && e.target.dataset.index % width !== 0 )){
+      e.target.classList.toggle('highlightOn')  
+      playerCells[adjacentValues[0]].classList.toggle('highlightOn')
+      playerCells[adjacentValues[1]].classList.toggle('highlightOn')
+      playerCells[adjacentValues[2]].classList.toggle('highlightOn')
+    } else if (boatSelection === 5 && ((e.target.dataset.index - 3) % width !== 0 && (e.target.dataset.index - 2) % width !== 0 && (e.target.dataset.index - 1) % width !== 0 && e.target.dataset.index % width !== 0)){
+      e.target.classList.toggle('highlightOn')
+      playerCells[adjacentValues[0]].classList.toggle('highlightOn')
+      playerCells[adjacentValues[1]].classList.toggle('highlightOn')
+      playerCells[adjacentValues[2]].classList.toggle('highlightOn')
+      playerCells[adjacentValues[3]].classList.toggle('highlightOn')
+    }
+  }
+
+  //Highlight vertical squares
+  function highlightVertical(e) {
+    const vertValues = [e.target.dataset.index - 10, e.target.dataset.index - 20, e.target.dataset.index - 30, e.target.dataset.index - 40, e.target.dataset.index - 50]
+    if (boatSelection === 1 && (e.target.dataset.index / width >= 1)){
+      e.target.classList.toggle('highlightOn')
+      playerCells[vertValues[0]].classList.toggle('highlightOn')
+    } else if (boatSelection === 2 && ((e.target.dataset.index - 10) / width >= 1 && e.target.dataset.index / width >= 1)){
+      e.target.classList.toggle('highlightOn')
+      playerCells[vertValues[0]].classList.toggle('highlightOn')
+      playerCells[vertValues[1]].classList.toggle('highlightOn')
+    } else if (boatSelection === 3 && ((e.target.dataset.index - 10) / width >= 1 && e.target.dataset.index / width >= 1)){
+      e.target.classList.toggle('highlightOn')
+      playerCells[vertValues[0]].classList.toggle('highlightOn')
+      playerCells[vertValues[1]].classList.toggle('highlightOn')
+    } else if (boatSelection === 4 && ((e.target.dataset.index - 20) / width >= 1 && (e.target.dataset.index - 10) / width >= 1 && e.target.dataset.index / width >= 1)){
+      e.target.classList.toggle('highlightOn')
+      playerCells[vertValues[0]].classList.toggle('highlightOn')
+      playerCells[vertValues[1]].classList.toggle('highlightOn')
+      playerCells[vertValues[2]].classList.toggle('highlightOn')
+    } else if (boatSelection === 5 && ((e.target.dataset.index - 30) / width >= 1 && (e.target.dataset.index - 20) / width >= 1 && (e.target.dataset.index - 10) / width >= 1 && e.target.dataset.index / width >= 1)){
+      e.target.classList.toggle('highlightOn')
+      playerCells[vertValues[0]].classList.toggle('highlightOn')
+      playerCells[vertValues[1]].classList.toggle('highlightOn')
+      playerCells[vertValues[2]].classList.toggle('highlightOn')
+      playerCells[vertValues[3]].classList.toggle('highlightOn')
+    } 
   }
 
   //Highlight squares
   function highlightSquares(e) {
     if (gameActive === 1 && horizontal === true) {
-      const adjacentValues = [e.target.dataset.index - 1, e.target.dataset.index - 2, e.target.dataset.index - 3, e.target.dataset.index - 4, e.target.dataset.index - 5]
-      if (boatSelection === 1 && (e.target.dataset.index % width !== 0)){
-        e.target.classList.toggle('highlightOn')
-        playerCells[adjacentValues[0]].classList.toggle('highlightOn')
-      } else if (boatSelection === 2 && ((e.target.dataset.index - 1) % width !== 0 && e.target.dataset.index % width !== 0)){
-        e.target.classList.toggle('highlightOn')
-        playerCells[adjacentValues[0]].classList.toggle('highlightOn')
-        playerCells[adjacentValues[1]].classList.toggle('highlightOn')
-      } else if (boatSelection === 3 && ((e.target.dataset.index - 1) % width !== 0 && e.target.dataset.index % width !== 0 )){
-        e.target.classList.toggle('highlightOn')
-        playerCells[adjacentValues[0]].classList.toggle('highlightOn')
-        playerCells[adjacentValues[1]].classList.toggle('highlightOn')
-      } else if (boatSelection === 4 && ((e.target.dataset.index - 2) % width !== 0 && (e.target.dataset.index - 1) % width !== 0 && e.target.dataset.index % width !== 0 )){
-        e.target.classList.toggle('highlightOn')  
-        playerCells[adjacentValues[0]].classList.toggle('highlightOn')
-        playerCells[adjacentValues[1]].classList.toggle('highlightOn')
-        playerCells[adjacentValues[2]].classList.toggle('highlightOn')
-      } else if (boatSelection === 5 && ((e.target.dataset.index - 3) % width !== 0 && (e.target.dataset.index - 2) % width !== 0 && (e.target.dataset.index - 1) % width !== 0 && e.target.dataset.index % width !== 0)){
-        e.target.classList.toggle('highlightOn')
-        playerCells[adjacentValues[0]].classList.toggle('highlightOn')
-        playerCells[adjacentValues[1]].classList.toggle('highlightOn')
-        playerCells[adjacentValues[2]].classList.toggle('highlightOn')
-        playerCells[adjacentValues[3]].classList.toggle('highlightOn')
-      }
+      checkForHighlighting()
+      highlightHorizontal(e)
     } else if (gameActive === 1 && horizontal === false) {
-      const vertValues = [e.target.dataset.index - 10, e.target.dataset.index - 20, e.target.dataset.index - 30, e.target.dataset.index - 40, e.target.dataset.index - 50]
-      if (boatSelection === 1 && (e.target.dataset.index / width >= 1)){
-        e.target.classList.toggle('highlightOn')
-        playerCells[vertValues[0]].classList.toggle('highlightOn')
-      } else if (boatSelection === 2 && ((e.target.dataset.index - 10) / width >= 1 && e.target.dataset.index / width >= 1)){
-        e.target.classList.toggle('highlightOn')
-        playerCells[vertValues[0]].classList.toggle('highlightOn')
-        playerCells[vertValues[1]].classList.toggle('highlightOn')
-      } else if (boatSelection === 3 && ((e.target.dataset.index - 10) / width >= 1 && e.target.dataset.index / width >= 1)){
-        e.target.classList.toggle('highlightOn')
-        playerCells[vertValues[0]].classList.toggle('highlightOn')
-        playerCells[vertValues[1]].classList.toggle('highlightOn')
-      } else if (boatSelection === 4 && ((e.target.dataset.index - 20) / width >= 1 && (e.target.dataset.index - 10) / width >= 1 && e.target.dataset.index / width >= 1)){
-        e.target.classList.toggle('highlightOn')
-        playerCells[vertValues[0]].classList.toggle('highlightOn')
-        playerCells[vertValues[1]].classList.toggle('highlightOn')
-        playerCells[vertValues[2]].classList.toggle('highlightOn')
-      } else if (boatSelection === 5 && ((e.target.dataset.index - 30) / width >= 1 && (e.target.dataset.index - 20) / width >= 1 && (e.target.dataset.index - 10) / width >= 1 && e.target.dataset.index / width >= 1)){
-        e.target.classList.toggle('highlightOn')
-        playerCells[vertValues[0]].classList.toggle('highlightOn')
-        playerCells[vertValues[1]].classList.toggle('highlightOn')
-        playerCells[vertValues[2]].classList.toggle('highlightOn')
-        playerCells[vertValues[3]].classList.toggle('highlightOn')
-      } 
+      checkForHighlighting()
+      highlightVertical(e)
     }
   }
 
-  //select function
-  function select() {
-    boatSelection++
-    console.log('BOAT SELECTION -->', boatSelection)
-  }
-
   //change boat orientation
-
   function boatOrientation(e) {
     const space = 32
     if (e.keyCode === space && gameActive === 1) {
@@ -142,6 +160,42 @@ function init() {
     }
   }
 
+  //select function
+  function confirmBoatPosition() {
+    playerCells.forEach
+    boatSelection++
+    highlightBoatPic()
+    console.log('BOAT SELECTION -->', boatSelection)
+  }
+
+  //highlight boat picture
+  function highlightBoatPic() {
+    if (boatSelection === 1) {
+      pPatrol.classList.add('highlightOn')
+    } else if (boatSelection === 2) {
+      pPatrol.classList.remove('highlightOn')
+      pSubmarine.classList.add('highlightOn')
+    } else if (boatSelection === 3) {
+      pSubmarine.classList.remove('highlightOn')
+      pDestroyer.classList.add('highlightOn')
+    } else if (boatSelection === 4) {
+      pDestroyer.classList.remove('highlightOn')
+      pBattleship.classList.add('highlightOn')
+    } else if (boatSelection === 5) {
+      pBattleship.classList.remove('highlightOn')
+      pCarrier.classList.add('highlightOn')
+    }
+  }
+
+  //Check squares for highlighting
+  function checkForHighlighting() {
+    playerCells.forEach(sqr => {
+      if (sqr.classList.contains('highlightOn')) {
+        sqr.classList.remove('highlightOn')
+      }
+    })
+  }
+
   // !EVENT LISTENERS
 
   //Start button
@@ -151,7 +205,7 @@ function init() {
   playerCells.forEach(sqr => {
     sqr.addEventListener('mouseover', highlightSquares)
     sqr.addEventListener('mouseleave', highlightSquares)
-    sqr.addEventListener('click', select)
+    sqr.addEventListener('click', confirmBoatPosition)
   })
 
   document.addEventListener('keydown', boatOrientation)
@@ -172,7 +226,7 @@ function init() {
 
 
 
-}  
+   
 
-
+}
 window.addEventListener('DOMContentLoaded', init)
